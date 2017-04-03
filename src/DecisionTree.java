1907f193ae4f5a862631d44c02a824cd0111645d
Aidan Doak <hc.adoak@gmail.com>
2017-04-03 17:06:26 +1200

    Moved the file reading to its own class to handle the training and testing data independently of each other. Added buildTree method and pseudocode.

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,5 +1,62 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by aidandoak on 28/03/17.
  */
 public class DecisionTree {
+
+    public DTNode buildTree(List<InstanceSet.Instance> instances, List<String> attributes){
+        if(instances.isEmpty())
+            // TODO: return the baseline predictor:
+            return new DTNode();
+        // Else if instances are pure:
+            // return node containing the name of the class of instances in the node and prob = 1
+
+        // Else if attributes empty
+            // return node containing the name and prob of the majority class of the instances in this node
+            // choose randomly if classes are equal
+
+        else { // Find best attribute:
+            for(int i = 0; i < attributes.size(); ++i){
+                Set<InstanceSet.Instance> trueSet = new HashSet<>();
+                Set<InstanceSet.Instance> falseSet = new HashSet<>();
+
+                for(InstanceSet.Instance instance : instances){
+                    boolean attributeTrue = instance.getAtt(i);
+
+                    // Add it to a set depending on its bool value:
+                    if(attributeTrue)
+                        trueSet.add(instance);
+                    else
+                        falseSet.add(instance);
+                }
+
+                // TODO: compute purity of each set
+
+                // If weighted avg of the sets is best so far:
+                    // bestAtt = attributes.get(i);
+                    // set of best true instances
+                    // set of best false instances
+            }
+
+            // TODO: build subtree using the remaining attributes (recursive!)
+
+            // Return the node with the best attribute, and its left and right
+            return new DTNode();
+        }
+    }
+
+    /* Main Method: */
+
+    public static void main(String[] args){
+        DecisionTree dt = new DecisionTree();
+
+        // Create the training data and build the tree from it:
+        InstanceSet trainingData = new InstanceSet();
+        trainingData.readDataFile(args[0]);
+        dt.buildTree(trainingData.allInstances, trainingData.attNames);
+
+    }
 }
---
+++ src/InstanceSet.java
@@ -0,0 +1,110 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aidandoak on 3/04/17.
+ */
+public class InstanceSet {
+    public int numCategories;
+    public int numAtts;
+    public List<String> categoryNames;
+    public List<String> attNames;
+    public List<Instance> allInstances;
+
+
+    public void readDataFile(String fname){
+    /* format of names file:
+     * names of categories, separated by spaces
+     * names of attributes
+     * category followed by true's and false's for each instance
+     */
+        System.out.println("Reading data from file "+fname);
+        try {
+            File data = new File(fname);
+            BufferedReader read = new BufferedReader(new FileReader(data));
+
+            categoryNames = new ArrayList<String>();
+            for(String s : read.readLine().split(" ")){
+                categoryNames.add(s);
+            }
+            numCategories = categoryNames.size();
+
+            attNames = new ArrayList<String>();
+            for(String s : read.readLine().split(" ")) {
+                attNames.add(s);
+            }
+            numAtts = attNames.size();
+
+            // Read the rest of the file as new instances:
+            allInstances = readInstances(read);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Data File caused IO exception");
+        }
+    }
+
+
+    private List<Instance> readInstances(BufferedReader reader){
+    /* instance = classname and space separated attribute values */
+        List<Instance> instances = new ArrayList<Instance>();
+
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                // Split the line by spaces
+                String[] sp = line.split(" ");
+                boolean[] values = new boolean[sp.length - 1];
+
+                // Turn all strings after the catagory name into booleans:
+                for(int i = 1; i < sp.length; ++i){
+                    values[i - 1] = Boolean.parseBoolean(sp[i]);
+                }
+
+                // Create the instance:
+                instances.add(new Instance(categoryNames.indexOf(sp[0]), values));
+            }
+            System.out.println("Read " + instances.size() + " instances");
+        } catch(IOException e){
+            System.out.println("Error creating instances from file.");
+        }
+        return instances;
+    }
+
+    /**
+     * A representation of an instance of data.
+     */
+    public class Instance {
+
+        private int category;
+        private List<Boolean> vals;
+
+        public Instance(int cat, boolean[] values){
+            category = cat;
+            vals = new ArrayList<Boolean>();
+
+            for(int i = 0; i < values.length; ++i)
+                vals.add(values[i]);
+        }
+
+        public boolean getAtt(int index){
+            return vals.get(index);
+        }
+
+        public int getCategory(){
+            return category;
+        }
+
+        public String toString(){
+            StringBuilder ans = new StringBuilder(categoryNames.get(category));
+            ans.append(" ");
+            for (Boolean val : vals)
+                ans.append(val?"true  ":"false ");
+            return ans.toString();
+        }
+
+    }
+}
